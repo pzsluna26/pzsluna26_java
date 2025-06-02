8d3d357399e92989e7861cf1ee6261b68e1158ed
pzsluna26 <pzsluna26@gmail.com>
2025-06-02 13:09:09 +0900

    upload

---
+++ src/data_structure_ch04/Test02.java
@@ -0,0 +1,171 @@
+package data_structure_ch04;
+import java.util.Random;
+import java.util.Scanner;
+
+//int형 고정 길이 스택
+
+class IntStack3 {
+	private int[] stk; // 스택용 배열
+	private int capacity; // 스택의 크기
+	private int top; // 스택 포인터
+
+
+	//--- 실행시 예외: 스택이 비어있음 ---//
+	public class EmptyIntStackException extends RuntimeException {
+		public EmptyIntStackException(String s) {
+			super(s); //RuntimeException의 생성자 호출
+		}
+	}
+	
+//--- 실행시 예외: 스택이 가득 참 ---//
+	public class OverflowIntStackException extends RuntimeException {
+		public OverflowIntStackException(String s) {
+		super(s);
+		}
+	}
+	
+//--- 생성자(constructor) ---//
+	public IntStack3(int maxlen) {
+		capacity = maxlen;
+		try{
+			stk = new int[capacity]; //heap에 메모리 할당
+		} 	catch (OutOfMemoryError e){
+			System.out.println("메모리 없음");
+			capacity = 0;
+		}	
+		top=0;
+	}
+
+//--- 스택에 x를 푸시 ---//
+	public int push(int x) throws OverflowIntStackException {
+		if (top >= capacity) // 스택이 가득 참
+			throw new OverflowIntStackException("push: stack overflow");
+		stk[top]=x;
+		top++;
+		return stk[top-1];
+	}
+
+//--- 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄) ---//
+	public int pop() throws EmptyIntStackException {
+		if (top <= 0) {
+			throw new EmptyIntStackException("pop: stack is empty");
+		}return stk[--top];	
+	}
+
+//--- 스택에서 데이터를 피크(peek, 정상에 있는 데이터를 들여다봄) ---//
+	public int peek() throws EmptyIntStackException {
+		if(top <= 0) {
+			throw new EmptyIntStackException("peek: take a brief look");
+		} return stk[top-1];
+		}
+
+//--- 스택을 비움 ---//
+	public void clear() {
+		top=0;
+	}
+
+//--- 스택에서 x를 찾아 인덱스(없으면 –1)를 반환 ---//
+	public int indexOf(int x) {
+		for(int i=top-1; i>=0; i--) {
+			if(stk[i] == x) {
+				return i;
+			}	
+		}	
+		return -1;
+	}
+	
+//--- 스택의 크기를 반환 ---//
+	public int getCapacity() {
+		return capacity;
+	}
+
+//--- 스택에 쌓여있는 데이터 갯수를 반환 ---//
+	public int size() {
+		return top;
+	}
+
+//--- 스택이 비어있는가? ---//
+	public boolean isEmpty() {
+		return top <=0;
+	}
+
+//--- 스택이 가득 찼는가? ---//
+	public boolean isFull() {
+		return top >= capacity;
+	}
+	
+//--- 스택 안의 모든 데이터를 바닥 → 정상 순서로 표시 ---//
+	public void dump() throws EmptyIntStackException{
+		if(top <= 0) {
+			throw new EmptyIntStackException("dump: 스택이 비었습니다");
+		
+		} else {
+			for(int i=0; i<top; i++) {
+				System.out.println(stk[i]+" ");
+				
+			}System.out.println();
+		}
+	}
+}
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		IntStack3 s = new IntStack3(4); // 최대 64 개를 푸시할 수 있는 스택
+		Random rand = new Random();
+		while (true) {
+			System.out.println(); // 메뉴 구분을 위한 빈 행 추가
+			System.out.printf("현재 데이터 개수: %d / %d\n", s.size(), s.getCapacity());
+			System.out.print("(1)push　(2)pop　(3)peek　(4)dump　(0)종료: ");
+
+			int menu = stdIn.nextInt();
+			if (menu == 0)
+				break;
+
+			int x;
+			switch (menu) {
+
+			case 1: // 푸시
+				System.out.print("데이터: ");
+				x = rand.nextInt(20);
+				try {
+					s.push(x);
+				} catch (IntStack3.OverflowIntStackException e) {
+					System.out.println("스택이 가득 찼습니다." + e.getMessage());
+					e.printStackTrace();
+				}
+				break;
+
+			case 2: // 팝
+				try {
+					x = s.pop();
+					System.out.println("팝한 데이터는 " + x + "입니다.");
+				} catch (IntStack3.EmptyIntStackException e) {
+					System.out.println("스택이 비어있습니다." + e.getMessage());
+					e.printStackTrace();
+				}
+				break;
+
+			case 3: // 피크
+				try {
+					x = s.peek();
+					System.out.println("피크한 데이터는 " + x + "입니다.");
+				} catch (IntStack3.EmptyIntStackException e) {
+					System.out.println("스택이 비어있습니다." + e.getMessage());
+					e.printStackTrace();
+				}
+				break;
+
+			case 4: // 덤프
+				try {
+					s.dump();
+				} catch (IntStack3.EmptyIntStackException e) {
+					System.out.println("스택이 비어있습니다." + e.getMessage());
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+	}
+}
