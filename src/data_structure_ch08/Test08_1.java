feb672666f56d9a436ccdf1b10aea099a29fd304
pzsluna26 <pzsluna26@gmail.com>
2025-06-13 17:59:07 +0900

    upload

--- src/data_structure_ch08/Test08_1.java
+++ src/data_structure_ch08/Test08_1.java
@@ -1,88 +1,119 @@
 package data_structure_ch08;
-
 //단순한 linked list에서 insert, delete하는 알고리즘을 코딩: 1단계
 
 import java.util.Random;
 import java.util.Scanner;
 
-class Node11 {
+import org.w3c.dom.Node;
+
+class Node1 {
 	int data;
-	Node11 link;
+	Node1 next;
 
-	Node11(int element) {
+	public Node1(int element) {
 		data = element;
-		link = null;
+		next = null;
 	}
 }
 
-class LinkedList11 {
-	Node11 first;
+class LinkedList1 {
+    Node1 first;
 
-	public LinkedList11() {
-		first = null;
-	}
+    public LinkedList1() {
+        first = null;
+    }
 
-	public boolean Delete(int element) //전달된 element 값이 존재 하면 삭제하고 true로 리턴
-	{
-		Node11 q, current = first;
-		q = current;
-		while (current != null) {
-			//구현 
+    public int Delete(int element) {
+        if (first == null)
+            return -1; // 리스트 비어 있음
 
-		}
-		return false;// 삭제할 대상이 없다.
-	}
+        Node1 p = first;
+        Node1 q = null;
 
-	public void Show() { // 전체 리스트를 순서대로 출력한다.
-		Node11 p = first;
-		int num = 0;
-		while (p != null) {
-			//구현 
-		}
-		System.out.println();
-	}
+        while (p != null) {
+            if (p.data == element) {
+                if (q == null) {
+                    first = p.next; // 첫 번째 노드 삭제
+                } else {
+                    q.next = p.next;
+                }
+                return 1; // 삭제 성공
+            }
+            q = p;
+            p = p.next;
+        }
+        return 0; // 삭제할 값 없음
+    }
 
-	public void Add(int element) // 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
-	{
-		Node11 newNode = new Node11(element);
-		if (first == null) // insert into empty list
-		{
-			first = newNode;
-			return;
-		}
-		// 기존 노드들이 있으면 순서가 유지하도록 삽입할 노드 위치를 찾는다
-		Node11 p = first, q = null;
-		while (p != null) {
-			//구현
-		}
-	}
+    public void Show() {
+        Node1 p = first;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
 
-	public boolean Search(int data) { //전달된 data 값을 찾아 존재하면 true로 리턴, 없으면 false로 리턴
-		Node11 ptr = first;
-		while (ptr != null) {
-			//구현
-		}
-		return false;
-	}
-	void Merge(LinkedList11 b) {
-		/*
-		 * 연결리스트 a,b에 대하여 a = a + b
-		 * merge하는 알고리즘 구현으로 in-place 방식으로 합병/이것은 새로운 노드를 만들지 않고 합병하는 알고리즘 구현
-		 * 난이도 등급: 최상
-		 * a = (3, 5, 7), b = (2,4,8,9)이면 a = (2,3,4,5,8,9)가 되도록 구현하는 코드
-		 */
-		Node11 p = first, q = b.first;
-		Node11 temp = null;
-		if(q.data < p.data) { //시작 노드 결정 
-			temp = first;
-			first = b.first;
-			b.first = temp;
-		}
-		while (p != null && q != null) {
-			//구현
-		}
-	}
+    public void Add(int element) {
+        Node1 newNode = new Node1(element);
+        Node1 p = first;
+        Node1 q = null;
+
+        while (p != null && p.data < element) {
+            q = p;
+            p = p.next;
+        }
+
+        if (q == null) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            newNode.next = q.next;
+            q.next = newNode;
+        }
+    }
+
+    public boolean Search(int data) {
+        Node1 p = first;
+        while (p != null) {
+            if (p.data == data)
+                return true;
+            p = p.next;
+        }
+        return false;
+    }
+
+    public void Merge(LinkedList1 b) {
+        Node1 p = this.first;
+        Node1 q = b.first;
+        Node1 head = null;
+        Node1 tail = null;
+
+        while (p != null && q != null) {
+            Node1 selected;
+            if (p.data <= q.data) {
+                selected = p;
+                p = p.next;
+            } else {
+                selected = q;
+                q = q.next;
+            }
+
+            if (head == null) {
+                head = tail = selected;
+            } else {
+                tail.next = selected;
+                tail = selected;
+            }
+        }
+
+        if (p != null) tail.next = p;
+        if (q != null) tail.next = q;
+
+        this.first = head;
+    }
 }
+
 public class Test08_1 {
 	enum Menu {
 		Add("삽입"), Delete("삭제"), Show("인쇄"), Search("검색"), Merge("합병"), Exit("종료");
@@ -90,8 +121,8 @@ public class Test08_1 {
 		private final String message; // 표시할 문자열
 
 		static Menu MenuAt(int idx) { // 순서가 idx번째인 열거를 반환
-			for (Menu m : Menu.values())
-				if (m.ordinal() == idx)
+			for (Menu m : Menu.values()) // m = menu의 벨류를 출력한 객체
+				if (m.ordinal() == idx) // m.ordinal 은 벨류를 인덱스로 변환하는 메서드
 					return m;
 			return null;
 		}
@@ -99,7 +130,6 @@ public class Test08_1 {
 		//생성자는 각 상수가 정의될 때 호출되며, 해당 상수의 message 필드를 초기화하는 역할
 		//enum 상수가 언제 정의되는가를 찾아 보아야 한다 
 		Menu(String string) { // 생성자(constructor)가 언제 호출되는지 파악하는 것이 필요하다 
-			//Menu.values()를 처음 실행할 때 생성자가 호출되지만, 이후의 호출에서는 호출되지 않는다.
 			message = string;
 			System.out.println("\nMenu 생성자 호출:: " + string);
 		}
@@ -114,7 +144,7 @@ public class Test08_1 {
 		Scanner sc = new Scanner(System.in);
 		int key;
 		do {
-			for (Menu m : Menu.values()) {//m은 열거형 상수가 정의될 때 생성자가 호출된다 
+			for (Menu m : Menu.values()) {
 				System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
 				//n%3은 3으로 나누어 나머지를 계산한다 
 				if ((m.ordinal() % 3) == 2 && m.ordinal() != Menu.Exit.ordinal())//메뉴 출력시에 다음행에 출력하라는 의미
@@ -129,10 +159,11 @@ public class Test08_1 {
 	public static void main(String[] args) {
 		Menu menu; // 메뉴 참조 변수일 뿐이다 
 		Random rand = new Random();
-		LinkedList11 l = new LinkedList11();
+		LinkedList1 l = new LinkedList1();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("추가할 난수 숫자 개수::");
 		int count = sc.nextInt(); //난수 생성 갯수
+
 		int data = 0;
 		do {
 			switch (menu = SelectMenu()) {//Menu 생성자 호출 - menu 객체를 리턴한다 
@@ -145,8 +176,13 @@ public class Test08_1 {
 			case Delete:
 				System.out.println("삭제할 값을 입력: ");
 				data = sc.nextInt();
-				boolean tag = l.Delete(data);
-				System.out.println("삭제 데이터 존재여부: " + tag);
+				int tag = l.Delete(data);
+				System.out.println("삭제 결과: " + switch (tag) {
+					case 1 -> "삭제 성공!";
+					case 0 -> "삭제할 값이 없습니다.";
+					case -1 -> "리스트가 비어 있습니다.";
+					default -> "알 수 없는 오류";
+				});
 				break;
 			case Show: //리스트 전체를 출력
 				l.Show();
@@ -159,8 +195,8 @@ public class Test08_1 {
 				else
 					System.out.println("검색 값 = " + n + " 데이터가 존재합니다.");
 				break;
-			case Merge://
-				LinkedList11 l2 = new LinkedList11();
+			case Merge://리스트 l과 l2를 합병하여 올림차순 정렬이 되게 구현한다 
+				LinkedList1 l2 = new LinkedList1();
 				for (int i =0; i < count; i++) {
 					data = rand.nextInt(20);
 					l2.Add(data);
